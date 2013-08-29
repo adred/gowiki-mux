ffc1c173555f418b6810720744bf97d978a59c6e
Red Adaya <red.adaya@gmail.com>
2013-08-29 16:59:10 +0800

    redo add and upate functions

--- wiki.go
+++ wiki.go
@@ -2,14 +2,13 @@ package main
 
 import (
     "fmt"
+    "os"
     "strconv"
     "html/template"
     "net/http"
     "github.com/gorilla/mux"
-    "regexp"
     "database/sql"
     _ "github.com/go-sql-driver/mysql"
-    "os"
 )
 
 type Page struct {
@@ -18,15 +17,26 @@ type Page struct {
     Body  string
 }
 
-func (p *Page) Add() error {
-    st, err := db.Prepare("INSERT INTO page VALUES (?, ?, ?)")
+func (p *Page) Add() int {
+    st, err := db.Prepare("INSERT INTO page VALUES ('', ?, ?)")
     defer st.Close()
     if err != nil{
         fmt.Print( err )
         os.Exit(1)
     }
-    st.Exec(p.ID, p.Title, p.Body)
-    return nil
+    res, err := st.Exec(p.Title, p.Body)
+    if err != nil{
+        fmt.Print( err )
+        os.Exit(1)
+    }
+    id, err := res.LastInsertId()
+    if err != nil{
+        fmt.Print( err )
+        os.Exit(1)
+    }
+    idFinal, _ := strconv.Atoi(strconv.FormatInt(id, 10))
+
+    return idFinal
 }
 
 func (p *Page) Update() error {
@@ -36,7 +46,17 @@ func (p *Page) Update() error {
         fmt.Print( err )
         os.Exit(1)
     }
-    st.Exec(p.Title, p.Body, p.ID)
+    res, err := st.Exec(p.Title, p.Body, p.ID)
+    if err != nil{
+        fmt.Print( err )
+        os.Exit(1)
+    }
+    _, err = res.RowsAffected()
+    if err != nil{
+        fmt.Print( err )
+        os.Exit(1)
+    }
+
     return nil
 }
 
@@ -60,17 +80,7 @@ func LoadPage(id int) (*Page, error) {
 }
 
 func AddHandler(w http.ResponseWriter, r *http.Request) {
-    st, err := db.Prepare("SELECT id FROM page ORDER BY id DESC LIMIT 1")
-        if err != nil {
-        fmt.Print( err )
-        os.Exit(1)
-    }
-    defer st.Close()
-    var id int
-    err = st.QueryRow().Scan(&id)
-    id += 1
-
-    p := &Page{ID: id}
+    p := &Page{}
     RenderTemplate(w, "add", p)
 }
 
@@ -78,17 +88,14 @@ func SaveHandler(w http.ResponseWriter, r *http.Request, id int) {
     title := r.FormValue("title")
     body := r.FormValue("body")
     action := r.FormValue("action")
-    p := &Page{ID: id, Title: title, Body: body}
     if action == "add" {
-        err := p.Add()
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
+        p := &Page{Title: title, Body: body}
+        id = p.Add()
     } else if action == "update" {
+        p := &Page{ID: id, Title: title, Body: body}
         err := p.Update()
         if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
+            http.Redirect(w, r, "/edit/"+strconv.Itoa(id), http.StatusFound)
             return
         }
     }
@@ -107,17 +114,13 @@ func ViewHandler(w http.ResponseWriter, r *http.Request, id int) {
 func EditHandler(w http.ResponseWriter, r *http.Request, id int) {
     p, err := LoadPage(id)
     if err != nil {
-        p = &Page{}
+        p = &Page{ID: id}
     }
     RenderTemplate(w, "edit", p)
 }
 
 func MakeHandler(fn func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-        if !idValidator.MatchString(mux.Vars(r)["id"]) {
-            http.NotFound(w, r)
-            return
-        }
         id, _ := strconv.Atoi(mux.Vars(r)["id"])
         fn(w, r, id)
     }
@@ -145,7 +148,6 @@ func OpenDB(driver string, username string, password string, dbname string) {
     }
 }
 
-var idValidator = regexp.MustCompile("^[0-9]+$")
 const templateDir, dataDir = "templates/", "data/"
 var templates = template.Must(template.ParseFiles(templateDir + "add.html", templateDir + "edit.html", templateDir + "view.html"))
 var db *sql.DB
@@ -155,9 +157,10 @@ func main() {
 
     r := mux.NewRouter()
     r.HandleFunc("/add", AddHandler)
-    r.HandleFunc("/view/{id}", MakeHandler(ViewHandler))
-    r.HandleFunc("/edit/{id}", MakeHandler(EditHandler))
-    r.HandleFunc("/save/{id}", MakeHandler(SaveHandler))
+    r.HandleFunc("/view/{id:[0-9]+}", MakeHandler(ViewHandler))
+    r.HandleFunc("/edit/{id:[0-9]+}", MakeHandler(EditHandler))
+    r.HandleFunc("/save/{id:[0-9]+}", MakeHandler(SaveHandler))
+    r.HandleFunc("/save", MakeHandler(SaveHandler))
     http.Handle("/", r)
     http.ListenAndServe(":8080", nil)
 }
